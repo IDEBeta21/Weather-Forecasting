7acc6b4c4d7904db8e881e910ae76d56d281a6c5
IDEBeta21 <deladiaian96@gmail.com>
2021-11-07 22:27:33 +0800

    added testInput.php and ready for ui test

--- components/key.php
+++ components/key.php
@@ -1,5 +1,5 @@
 <?php
-   require_once("vendor/autoload.php");
+    require_once("vendor/autoload.php");
     $key = 'fce11b6c487a4e898fe142933211809';
 
     $client = new WeatherAPILib\WeatherAPIClient($key);
