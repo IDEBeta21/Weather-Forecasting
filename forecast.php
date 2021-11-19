6602b6401815158dddeb9feb87e557d01bc52406
IDEBeta21 <deladiaian96@gmail.com>
2021-11-19 23:19:08 +0800

    Forecast separation from the index.php

---
+++ forecast.php
@@ -0,0 +1,59 @@
+<div class="card-container">
+  <div class="center-card-container">
+    <div class="card">
+      <h5>Thu 30</h5>
+      <center><img src="src/icon-lightRain.png" id="img-card" alt="Light Rain"></center>
+      <h2>31&#x2103;</h2>
+      <p class="status">Light Rain</p>
+    </div>
+
+    <div class="card">
+      <h5>Fri 01</h5>
+      <center><img src="src/icon-partlySunny.png" id="img-card" alt="Partly Sunny"></center>
+      <h2>32&#x2103;</h2>
+      <p class="status">Partly Sunny</p>
+    </div>
+
+    <div class="card">
+      <h5>Sat 02</h5>
+      <center><img src="src/icon-thunderstorms.png" id="img-card" alt="Thunderstorms"></center>
+      <h2>32&#x2103;</h2>
+      <p class="status">Thunderstorm</p>
+    </div>
+
+    <div class="card">
+      <h5>Sun 03</h5>
+      <center><img src="src/icon-lightRain.png" id="img-card" alt="Light Rain"></center>
+      <h2>31&#x2103;</h2>
+      <p class="status">Light Rain</p>
+    </div>
+
+    <div class="card">
+      <h5>Mon 04</h5>
+      <center><img src="src/icon-lightRain.png" id="img-card" alt="Light Rain"></center>
+      <h2>32&#x2103;</h2>
+      <p class="status">Light Rain</p>
+    </div>
+
+    <div class="card">
+      <h5>Tue 05</h5>
+      <center><img src="src/icon-lightRain.png" id="img-card" alt="Light Rain"></center>
+      <h2>31&#x2103;</h2>
+      <p class="status">Light Rain</p>
+    </div>
+
+    <div class="card">
+      <h5>Wed 06</h5>
+      <center><img src="src/icon-lightRain.png" id="img-card" alt="Light Rain"></center>
+      <h2>30&#x2103;</h2>
+      <p class="status">Rain</p>
+    </div>
+
+    <div class="card">
+      <h5>Thu 07</h5>
+      <center><img src="src/icon-rain.png" id="img-card" alt="Rain"></center>
+      <h2>31&#x2103;</h2>
+      <p class="status">Rain</p>
+    </div>
+  </div>
+</div>
